4198b2e25acf3736a32a565108e91fe0557a13a9
Efsun Yusubzade <166283682+TelimEfsun@users.noreply.github.com>
2025-05-02 11:29:32 +0400

    Add files via upload

---
+++ Lesson 1.sql
@@ -0,0 +1,22 @@
+﻿
+--create table Employees(
+--Id int identity (1,1) not null unique,
+--FirstName nvarchar(20),
+--LastName nvarchar(20),
+--Email nvarchar(200),
+--HireDate date 
+--)
+
+--alter table Employees
+--add PhoneNumber nvarchar(20)
+
+--select * from Employees
+--insert into Employees(FirstName,LastName,Email,HireDate,PhoneNumber)
+--values 
+--('Orxan','Məmmədov','orxan.mammadov@example.com','2023-09-01', '+994501234567')
+
+--select * from Employees
+
+--update Employees set PhoneNumber='+994507654321' where Id=2
+
+--delete Employees where Id=1
